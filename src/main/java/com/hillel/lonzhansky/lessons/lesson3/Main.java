10a7d450cf24cefc09a2f588ab078e9ee7e2ff69
Denys Lonzhanskyi <lonzhansky@gmail.com>
2023-02-06 21:02:15 +0200

    test2

--- src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
@@ -31,10 +31,6 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
 
 
     }
